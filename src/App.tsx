a825cb11c11988388a9c33eb72e31bd9815f6b1a
Manga <mangab617@gmail.com>
2025-02-20 18:47:12 +0530

    Delete Task

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import TaskForm from './components/Task';
 import TaskList from './components/TaskList';
 import { Task } from './TaskType';
 
-const App = () => {
+const App: React.FC = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   const addTask = (task: Task) => {
     setTasks([...tasks, task])
     
     console.log('tasks', tasks);
+  };
+
+  const deleteTask = (id:Number) => {
+    setTasks(tasks.filter((task)=> task.id !== id));
+    console.log("delete", tasks);
+
   }
+
   return (
     <div>
       <TaskForm addTask={addTask}/>
-      <TaskList tasks={tasks}/>
+      <TaskList tasks={tasks} deleteTask={deleteTask}/>
     </div>
   );
 }
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,17 +1,23 @@
 
 import React from "react";
 import { Task } from "../TaskType";
+import Diamond from '../assets/delete.png'
 
 interface TaskListProps {
     tasks: Task[];
+    deleteTask: (id:number) => void;
 }
-const TaskList: React.FC<TaskListProps> = ({tasks}) => {
+const TaskList: React.FC<TaskListProps> = ({tasks, deleteTask}) => {
  console.log(tasks, 'tasks');
 
     return (
             <ul>
             {tasks.map((task) => (
-                <li key={task.id}>{task.title}</li>
+                <li key={task.id}>{task.title}
+                    <button onClick={()=> deleteTask(task.id)}>
+                        <img src={Diamond} alt={task.title} />
+                    </button>
+                </li>
             ))}
             </ul>
            
