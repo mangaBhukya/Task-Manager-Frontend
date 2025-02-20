dc71f28a901511e127a937314ba098751bfd9fe2
Manga <mangab617@gmail.com>
2025-02-20 15:47:43 +0530

    setup and basic task form

---
+++ src/components/Task.tsx
@@ -0,0 +1,21 @@
+
+const TaskForm  = () => {
+
+    return (
+        <form>
+            <input
+            type = 'text'
+            placeholder= 'Enter task'
+            />
+            
+            <button>
+             Add Task
+            </button>
+        </form>
+
+    );
+
+}
+
+export default TaskForm;
+
